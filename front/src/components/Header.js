12b69ad43e19539c8d68ecd0a73afd8644302ab6
jeong-mi <wjdal6951@naver.com>
2022-03-17 18:43:32 +0900

    merge request

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,15 +1,15 @@
-import React, { useContext, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import Nav from "react-bootstrap/Nav";
-import { UserStateContext, DispatchContext } from "../App";
-import { Col, Container, Navbar, Row } from "react-bootstrap";
+import React, { useContext, useEffect } from "react"
+import { useNavigate, useLocation } from "react-router-dom"
+import Nav from "react-bootstrap/Nav"
+import { UserStateContext, DispatchContext } from "../App"
+import { Col, Container, Navbar, Row } from "react-bootstrap"
 
 function Header() {
-    const navigate = useNavigate();
-    const location = useLocation();
+    const navigate = useNavigate()
+    const location = useLocation()
 
-    const userState = useContext(UserStateContext);
-    const dispatch = useContext(DispatchContext);
+    const userState = useContext(UserStateContext)
+    const dispatch = useContext(DispatchContext)
 
     // 전역상태에서 user가 null이 아니라면 로그인 성공 상태임.
     const isLogin = !!userState.user;
@@ -17,13 +17,12 @@ function Header() {
     // 로그아웃 클릭 시 실행되는 함수
     const logout = () => {
     // sessionStorage 에 저장했던 JWT 토큰을 삭제함.
-    sessionStorage.removeItem("userToken");
+    sessionStorage.removeItem("userToken")
     // dispatch 함수를 이용해 로그아웃함.
-    dispatch({ type: "LOGOUT" });
+    dispatch({ type: "LOGOUT" })
     // 기본 페이지로 돌아감.
-    navigate("/");
+    navigate("/")
     };
-
 
     return (
     <Navbar activeKey={location.pathname} bg="light">
