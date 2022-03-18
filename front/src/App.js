59ba6c50545511e4fc184568314cabce89473815
jeong-mi <wjdal6951@naver.com>
2022-03-18 14:05:10 +0900

    App.js 에러 고치기

--- front/src/App.js
+++ front/src/App.js
@@ -14,35 +14,16 @@ export const UserStateContext = createContext(null)
 export const DispatchContext = createContext(null)
 
 function App() {
-<<<<<<< HEAD
-  // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
-  const [userState, dispatch] = useReducer(loginReducer, {
-    user: null,
-  })
-=======
+
     // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
     const [userState, dispatch] = useReducer(loginReducer, {
         user: null,
     });
->>>>>>> c0a3dc29e9d9db97e3e527b594127108447faea2
 
     // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
     // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
     const [isFetchCompleted, setIsFetchCompleted] = useState(false);
 
-<<<<<<< HEAD
-  const fetchCurrentUser = async () => {
-    try {
-      // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
-      const res = await Api.get("user/current");
-      const currentUser = res.data
-
-      // dispatch 함수를 통해 로그인 성공 상태로 만듦.
-      dispatch({
-        type: "LOGIN_SUCCESS",
-        payload: currentUser,
-      })
-=======
     const fetchCurrentUser = async () => {
         try {
             // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
@@ -54,7 +35,6 @@ function App() {
                 type: "LOGIN_SUCCESS",
                 payload: currentUser,
             });
->>>>>>> c0a3dc29e9d9db97e3e527b594127108447faea2
 
             console.log("%c sessionStorage에 토큰 있음.", "color: #d93d1a;");
         } catch {
@@ -72,38 +52,6 @@ function App() {
     if (!isFetchCompleted) {
         return "loading...";
     }
-<<<<<<< HEAD
-    // fetchCurrentUser 과정이 끝났으므로, isFetchCompleted 상태를 true로 바꿔줌
-    setIsFetchCompleted(true);
-  }
-
-  // useEffect함수를 통해 fetchCurrentUser 함수를 실행함.
-  useEffect(() => {
-    fetchCurrentUser()
-  }, [])
-
-  if (!isFetchCompleted) {
-    return "loading..."
-  }
-
-  return (
-    <DispatchContext.Provider value={dispatch}>
-      <UserStateContext.Provider value={userState}>
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" exact element={<Portfolio />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegisterForm />} />
-            <Route path="/users/:userId" element={<Portfolio />} />
-            <Route path="/network" element={<Network />} />
-            <Route path="*" element={<Portfolio />} />
-          </Routes>
-        </Router>
-      </UserStateContext.Provider>
-    </DispatchContext.Provider>
-  )
-=======
 
     return (
         <DispatchContext.Provider value={dispatch}>
@@ -122,7 +70,6 @@ function App() {
             </UserStateContext.Provider>
         </DispatchContext.Provider>
     );
->>>>>>> c0a3dc29e9d9db97e3e527b594127108447faea2
 }
 
 export default App;
