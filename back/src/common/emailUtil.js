9ba66a3727e9c5f43345ff5061f660100141eb89
s0n9h2 <s0n9h2@gmail.com>
2022-03-22 18:02:01 +0900

    chore : fix comments

--- back/src/common/emailUtil.js
+++ back/src/common/emailUtil.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer")
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-let number = Math.floor(Math.random() * 1000000)+100000 // 난수 생성
-if (number>1000000) {
+let number = Math.floor(Math.random() * 1000000) + 100000 // 난수 인증번호 생성
+if (number > 1000000) {
     number = number - 100000;
 }
 
@@ -13,18 +13,18 @@ const transporter = nodemailer.createTransport({
     secure: true, // true for 587, false for other ports
     requireTLS: true,
     auth: {
-        user: process.env.GMAIL_ID, //gmail주소입력
-        pass: process.env.GMAIL_PASSWORD //gmail패스워드 입력
+        user: process.env.GMAIL_ID, // .env 파일 GMAIL_ID 변수 = gmail 주소
+        pass: process.env.GMAIL_PASSWORD // .env 파일 GMAIL_PASSWORD 변수 = gmail 패스워드
     }
 })
 
 const emailUtil = {
     sendEmail: async(email) => {
         await transporter.sendMail({   
-            from: process.env.GMAIL_ID, //보내는 주소 입력
-            to: email, //위에서 선언해준 받는사람 이메일
-            subject: '안녕하세요', //메일 제목
-            html: //내용
+            from: process.env.GMAIL_ID, // 보내는 주소 입력
+            to: email, // 위에서 선언해준 받는사람 이메일
+            subject: '안녕하세요', // 메일 제목
+            html: // 내용
                 `<p>아래 인증번호를 입력해주세요.</p>` +
                 `<b>${number}</b>`,
         })
