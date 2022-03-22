cb2dbc8b970811cacf020d0de1797bec960220f5
s0n9h2 <s0n9h2@gmail.com>
2022-03-23 00:43:27 +0900

    chore : category findOne -> find, login_required remove return

--- back/src/db/models/User.js
+++ back/src/db/models/User.js
@@ -21,31 +21,31 @@ const User = {
         return users
     },
 
-  update: async ({ userId, updateObject }) => {
-    const filter = { id: userId }
-    const update = { $set: updateObject }
-    const option = { returnOriginal: false }
+    update: async ({ userId, updateObject }) => {
+        const filter = { id: userId }
+        const update = { $set: updateObject }
+        const option = { returnOriginal: false }
 
-    const updatedUser = await UserModel.findOneAndUpdate(
-      filter,
-      update,
-      option
-    )
-    return updatedUser
-  },
-  
-  updatePassword: async ({ email, newPassword }) => {
-    const filter = { email }
-    const update = { password: newPassword }
-    const option = { runValidators: true }
-
-    const updatedUser = await UserModel.findOneAndUpdate(
+        const updatedUser = await UserModel.findOneAndUpdate(
         filter,
         update,
         option
-    )
-    return updatedUser
-}
+        )
+        return updatedUser
+    },
+    
+    updatePassword: async ({ email, newPassword }) => {
+        const filter = { email }
+        const update = { password: newPassword }
+        const option = { runValidators: true }
+
+        const updatedUser = await UserModel.findOneAndUpdate(
+            filter,
+            update,
+            option
+        )
+        return updatedUser
+    }
 }
 
 module.exports = { User }
