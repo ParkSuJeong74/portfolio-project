f6461e30e134e9b5bec95d09b306411fe9d92d22
s0n9h2 <s0n9h2@gmail.com>
2022-03-24 18:06:51 +0900

    bug : change user_id to userId in line 19

--- back/src/middlewares/login_required.js
+++ back/src/middlewares/login_required.js
@@ -15,7 +15,8 @@ const login_required = (req, res, next) => {
     try {
         const secretKey = process.env.JWT_SECRET_KEY || "secret-key"
         const jwtDecoded = jwt.verify(userToken, secretKey)
-        const userId = jwtDecoded.user_id
+        console.log(jwtDecoded)
+        const userId = jwtDecoded.userId
         req.currentUserId = userId
         next()
     } catch (error) {
