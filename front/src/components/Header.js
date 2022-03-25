b22da3e8ac172b18f6206bcabefc72e6337fd1a8
jeong-mi <wjdal6951@naver.com>
2022-03-26 04:41:26 +0900

    폰트수정, 거의 다 완료

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,8 +1,9 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { useNavigate, useLocation } from "react-router-dom"
 import { UserStateContext, DispatchContext } from "../App"
 import * as Api from '../api.js'
 import Style from '../App.module.css'
+import WithdrawModal from "./WithdrawModal"
 
 function Header() {
     const navigate = useNavigate()
@@ -22,14 +23,12 @@ function Header() {
         userDispatch({ type: "LOGOUT" });
     };
 
-    async function withdraw(){
-        console.log(userState.user?.id)
-        await Api.delete(`user/${userState.user?.id}`)
+    
 
-        console.log('회원이 탈퇴되었습니다.')
-        // 로그인 페이지로 이동함.
-        navigate("/login");
-    }
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const naviagationInformations = [
         {title: 'HOME', link: '/'},
@@ -38,7 +37,7 @@ function Header() {
     ]
 
     return (
-
+    <>
         <div className={Style.navBar}>
             <div style= {{fontSize: '2.5em'}}>MY PORTFOLIO</div>
             <ul style={{ gap: 30 }} className={Style.navItems}>
@@ -58,11 +57,13 @@ function Header() {
                     
                 )}
                 {isLogin && (
-                    <li onClick={withdraw}
+                    <li onClick={handleShow}
                         className = {Style.navWithdraw}>탈퇴</li>
                 )}
             </ul>
         </div>
+        <WithdrawModal show={show} handleClose={handleClose}/>
+    </>
 
     );
 
