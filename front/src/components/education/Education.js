c10b4081da2fb2e180e4741840dc6ed1f660a5b2
rnrn99 <giu0213@naver.com>
2022-04-03 21:40:24 +0900

    feat : mui 추가

--- front/src/components/education/Education.js
+++ front/src/components/education/Education.js
@@ -1,38 +1,33 @@
-import React, {useState} from 'react'
-import EducationCard from './EducationCard'
-import EducationEditForm from './EducationEditForm'
-import * as Api from '../../api'
+import React, { useState } from "react";
+import EducationCard from "./EducationCard";
+import EducationEditForm from "./EducationEditForm";
+import { Dialog, DialogTitle, DialogContent } from "@mui/material";
 
-function Education({education, isEditable, setEducations}){
-    const [isEditing, setIsEditing] = useState(false)
+function Education({ education, isEditable, setEducations }) {
+  const [isEditing, setIsEditing] = useState(false); // 편집 버튼 클릭 상태를 저장합니다.
 
-    const removeEducation = async () => {
-        try{
-            await Api.delete(`education/${education.id}`)
-            setEducations(prev => prev.filter(item => item.id !== education.id))
-        } catch (error){
-            alert(error.response.data)
-        }
-    }
-    
-    return (
-        <>
-            {isEditing ? (
-                <EducationEditForm 
-                    setEducations = {setEducations}
-                    currentEducation = {education}
-                    setIsEditing={setIsEditing}
-                    />
-            ) : (
-                <EducationCard 
-                    setIsEditing={setIsEditing}
-                    education={education}
-                    isEditable={isEditable}
-                    removeEducation={removeEducation}
-                />
-            )}
-        </>
-    )
+  return (
+    <>
+      <EducationCard
+        setIsEditing={setIsEditing}
+        education={education}
+        isEditable={isEditable}
+        setEducations={setEducations}
+      />
+      {isEditing && (
+        <Dialog open={isEditing} onClose={() => setIsEditing((cur) => !cur)}>
+          <DialogTitle>학력 편집</DialogTitle>
+          <DialogContent>
+            <EducationEditForm
+              setEducations={setEducations}
+              currentEducation={education}
+              setIsEditing={setIsEditing}
+            />
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
+  );
 }
 
-export default Education
+export default Education;
