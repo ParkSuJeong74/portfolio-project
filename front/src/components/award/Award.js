09ae39fd97eccbc6a0af1b354789d8a47043a925
Yujin <ywtv3636@naver.com>
2022-04-04 00:15:34 +0900

    feat : 수상이력mui

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -1,35 +1,33 @@
 import React, { useState} from 'react'
 import AwardCard from './AwardCard'
 import AwardEditForm from './AwardEditForm'
-import * as Api from '../../api'
+import { Dialog, DialogTitle, DialogContent } from "@mui/material";
 
 function Award({award, setAwards, isEditable}) {
     const [isEditing, setIsEditing] = useState(false)
 
-    const removeAward = async() => {
-        try{
-            await Api.delete(`award/${award.id}`)
-            setAwards(prev => prev.filter(item => item.id !== award.id))
-            
-        } catch(error) {
-            alert(error.response.data)
-        }
-    }
+    
 
     return (
         <>
-            {isEditing ? (
-                <AwardEditForm 
-                    setAwards = {setAwards}
-                    currentAward = {award}
-                    setIsEditing={setIsEditing}
-                />
-            ) : (
-                <AwardCard 
-                    setIsEditing={setIsEditing}
-                    removeAward={removeAward}
-                    award={award}
-                    isEditable={isEditable}/>
+           <AwardCard 
+                setIsEditing={setIsEditing}
+                award={award}
+                isEditable={isEditable}
+                setAwards={setAwards}
+            />
+                    
+            {isEditing &&(
+                <Dialog open={isEditing} onClose={() => setIsEditing((cur) => !cur)}>
+                    <DialogTitle>수상이력 편집</DialogTitle>
+                    <DialogContent>
+                        <AwardEditForm 
+                            setAwards = {setAwards}
+                            currentAward = {award}
+                            setIsEditing={setIsEditing}
+                         />
+                    </DialogContent>
+                </Dialog>
             )}
         </>
     )
