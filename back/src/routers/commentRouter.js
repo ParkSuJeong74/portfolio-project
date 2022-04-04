d35fa538dfa00eeb16552e05644d89ffcd046283
ParkSuJeong <sujeong9158@gmail.com>
2022-04-04 18:07:54 +0900

    chore: user api 수정

--- back/src/routers/commentRouter.js
+++ back/src/routers/commentRouter.js
@@ -1,89 +1,88 @@
-const is = require('@sindresorhus/is')
-const { Router } = require('express')
-const { login_required } = require('../middlewares/login_required')
-const { CommentService } = require('../services/commentService')
-const { User } = require('../db/models/User')
+const is = require("@sindresorhus/is")
+const { Router } = require("express")
+const { login_required } = require("../middlewares/login_required")
+const { CommentService } = require("../services/commentService")
+const { User } = require("../db/models/User")
 
 const commentRouter = Router()
 
 commentRouter.use(login_required)
 
 // POST : 댓글 작성
-commentRouter.post('/create', async (req, res, next) => {
-    try {  
-        if (is.emptyObject(req.body)) {
-            throw new Error(
-                "headers의 Content-Type을 application/json으로 설정해주세요"
-            )
-        }
-
-        //const userId = req.currentUserId
-        //const writerId = userId // 작성자 = 현재 로그인한 사용자 
-        const { userId, articleId, comment, hidden } = req.body
-        const newComment = await CommentService.addComment ({
-            userId,
-            writerId: userId,
-            articleId,
-            comment,
-            hidden
-        })
-
-        res.status(201).json(newComment)
-    } catch(err) {
-        next(err)
+commentRouter.post("/", async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      )
     }
+
+    //const userId = req.currentUserId
+    //const writerId = userId // 작성자 = 현재 로그인한 사용자
+    const { userId, articleId, comment, hidden } = req.body
+    const newComment = await CommentService.addComment({
+      userId,
+      writerId: userId,
+      articleId,
+      comment,
+      hidden,
+    })
+
+    res.status(201).json(newComment)
+  } catch (err) {
+    next(err)
+  }
 })
 
 // PUT : 댓글 수정
-commentRouter.put('/:id', async (req, res, next) => {
-    try {
-        //const userId = req.currentUserId
-        const commentId = req.params.id
-        const { userId, comment, hidden } = req.body
-        let writerName
+commentRouter.put("/:id", async (req, res, next) => {
+  try {
+    //const userId = req.currentUserId
+    const commentId = req.params.id
+    const { userId, comment, hidden } = req.body
+    let writerName
 
-        if(hidden == true) { // 익명 처리
-            writerName = '익명'
-        }
-        else {
-            const user = await User.findById({ userId })
-            console.log(user)
-            writerName = user.nickname // 닉네임으로 출력
-        }
-
-        const toUpdate = { comment, hidden, writerName }
-        const newComment = await CommentService.setComment({ userId, commentId, toUpdate })
-
-        if(newComment.errorMessage){
-            throw new Error(newComment.errorMessage)
-        }
-
-        res.status(200).send(newComment)
-    } catch(err) {
-        next(err)
+    if (hidden == true) {
+      // 익명 처리
+      writerName = "익명"
+    } else {
+      const user = await User.findById({ userId })
+      console.log(user)
+      writerName = user.nickname // 닉네임으로 출력
     }
+
+    const toUpdate = { comment, hidden, writerName }
+    const newComment = await CommentService.setComment({
+      userId,
+      commentId,
+      toUpdate,
+    })
+
+    res.status(200).send(newComment)
+  } catch (err) {
+    next(err)
+  }
 })
 
-
 // DELETE : 댓글 삭제(soft delete)
-commentRouter.put('/:id/delete', async (req, res, next) => {
-    try {
-        const userId = req.body.userId
-        const commentId = req.params.id
-        const { comment } = req.body
-        const isDeleted = true
+commentRouter.put("/:id/delete", async (req, res, next) => {
+  try {
+    const userId = req.body.userId
+    const commentId = req.params.id
+    const { comment } = req.body
+    const isDeleted = true
 
-        const toUpdate = { isDeleted, comment }
-        const newComment = await CommentService.deleteComment({ userId, commentId, toUpdate })
+    const toUpdate = { isDeleted, comment }
+    const newComment = await CommentService.deleteComment({
+      userId,
+      commentId,
+      toUpdate,
+    })
 
-        if(newComment.errorMessage){
-            throw new Error(newComment.errorMessage)
-        }
-
-        res.status(200).send(newComment)
-    } catch(err) {
-        next(err)
-    }
+    res.status(200).send(newComment)
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = { commentRouter }
--- back/src/services/categoryService.js
+++ back/src/services/categoryService.js
@@ -1,50 +1,54 @@
 const { Category } = require("../db")
 const { v4: uuidv4 } = require("uuid")
-const { SetUtil } = require('../common/setUtil')
+const { SetUtil } = require("../common/setUtil")
 
 const CategoryService = {
-    addCategory : async ({ userId, name, description }) => {
-        const id = uuidv4()
-        const category = await Category.findByName({ name })
-        if(category) {
-            throw new Error("같은 이름의 게시판이 이미 존재합니다.")
-        }
-
-        const newCategory = { id, userId, name, description }
-        const createNewCategory = await Category.create({ newCategory })
-
-        return createNewCategory
-    },
-
-    getCategory : async() => {
-        const category = await Category.findAll({})
-        return category
-    },
-
-    getCategoryByName : async({ name }) => {
-        const category = await Category.findAllByName({ name })
-        
-        if(!name){
-            throw new Error("해당 Name를 가진 수상 데이터는 없습니다. 다시 한 번 확인해 주세요.")
-        }
-        return category
-    },
-
-    setCategory : async({ categoryName, toUpdate }) => {
-        let category = await Category.findByName({ name: categoryName }) // 원래 카테고리 데이터
-        if(!category){
-            throw new Error("해당 Name를 가진 수상 데이터는 없습니다. 다시 한 번 확인해 주세요.")
-        }  
-        let categoryTest = await Category.findAllByName(toUpdate.name)
-        if([categoryTest].length > 1) {
-            throw new Error("같은 이름의 게시판이 이미 존재합니다.")
-        }
-
-        const updateObject = SetUtil.compareValues(toUpdate, category)
-        category = await Category.update({ categoryName, updateObject })
-        
-        return category
+  addCategory: async ({ userId, name, description }) => {
+    const id = uuidv4()
+    const category = await Category.findByName({ name })
+    if (category) {
+      throw new Error("같은 이름의 게시판이 이미 존재합니다.")
     }
+
+    const newCategory = { id, userId, name, description }
+    const createNewCategory = await Category.create({ newCategory })
+
+    return createNewCategory
+  },
+
+  getCategory: async () => {
+    const category = await Category.findAll({})
+    return category
+  },
+
+  getCategoryById: async ({ categoryId }) => {
+    const category = await Category.findAllById({ categoryId })
+
+    if (!name) {
+      throw new Error(
+        "해당 Name를 가진 수상 데이터는 없습니다. 다시 한 번 확인해 주세요."
+      )
+    }
+    return category
+  },
+
+  setCategory: async ({ categoryId, toUpdate }) => {
+    let category = await Category.findById({ categoryId }) // 원래 카테고리 데이터
+    if (!category) {
+      throw new Error(
+        "해당 Name를 가진 수상 데이터는 없습니다. 다시 한 번 확인해 주세요."
+      )
+    }
+    let categoryTest = await Category.findAllByName(toUpdate.name)
+    if ([categoryTest].length > 1) {
+      throw new Error("같은 이름의 게시판이 이미 존재합니다.")
+    }
+
+    const updateObject = SetUtil.compareValues(toUpdate, category)
+    category = await Category.update({ categoryId, updateObject })
+
+    return category
+  },
 }
 
 module.exports = { CategoryService }
