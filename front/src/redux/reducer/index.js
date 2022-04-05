5f2474dfc21bca51f6643130a035610166ca6184
rnrn99 <giu0213@naver.com>
2022-04-05 16:51:46 +0900

    chore : reducer combine

--- front/src/redux/reducer/index.js
+++ front/src/redux/reducer/index.js
@@ -1,14 +1,8 @@
 import { combineReducers } from "redux"
 import user from "./userReducer"
-import category from "./categoryReducer"
-import article from "./articleReducer"
-import comment from "./commentReducer"
 
 const rootReducer = combineReducers({
   user,
-  category,
-  article,
-  comment,
 })
 
 export default rootReducer
