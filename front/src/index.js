78a2808fca51d59b25148a9ad4e6d6bd60259f03
rnrn99 <giu0213@naver.com>
2022-04-05 11:27:25 +0900

    feat : redux 초기 설정

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-datepicker/dist/react-datepicker.css";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "react-datepicker/dist/react-datepicker.css"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import promiseMiddleware from "redux-promise"
+import ReduxThunk from "redux-thunk"
+import Reducer from "./redux/reducer"
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk,
+)(createStore)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__(),
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById("root"),
+)
