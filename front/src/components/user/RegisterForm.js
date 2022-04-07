6a9977cbf8d9618b13e2317075a632a8d396185c
hajun <audgkwns44@gmail.com>
2022-04-08 01:31:44 +0900

    refine registerForm

--- front/src/components/user/RegisterForm.js
+++ front/src/components/user/RegisterForm.js
@@ -1,11 +1,24 @@
 import React, { useState, useCallback } from "react"
 import { useNavigate } from "react-router-dom"
-import { Col, Row, Form, Button } from "react-bootstrap"
-import { Container } from "@mui/material"
+import {
+  Container,
+  Avatar,
+  Typography,
+  Box,
+  Grid,
+  TextField,
+  CssBaseline,
+  Button,
+  Link,
+} from "@mui/material"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
 
 import * as Api from "../../api"
 
 import EmailAuthModal from "./EmailAuthModal"
+
+const theme = createTheme()
 
 function RegisterForm() {
   const navigate = useNavigate()
@@ -84,9 +97,189 @@ function RegisterForm() {
   const handleModalShow = () => setIsModalActive(true)
 
   return (
-    <>
-      <Container>
-        <Row className="justify-content-md-center mt-5">
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            mb: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "#000d3e" }}>
+            <LockOutlinedIcon></LockOutlinedIcon>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Born as a new PUPA
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={9} sx={{ mb: 0 }}>
+                <TextField
+                  name="email"
+                  id="email"
+                  label="메일주소"
+                  required
+                  fullWidth
+                  autoFocus
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Button
+                  sx={{
+                    bgcolor: "#000d3e",
+                    color: "#FFF",
+                  }}
+                  onClick={() => {
+                    handleModalShow()
+                    onClickEmailAuthentication()
+                  }}
+                >
+                  이메일 인증
+                </Button>
+              </Grid>
+              {!isEmailValid && (
+                <Grid item xs={12} className="text-success">
+                  이메일 형식이 올바르지 않습니다.
+                </Grid>
+              )}
+              <Grid item xs={9}>
+                <TextField
+                  name="emailCertificate"
+                  id="emailCertificate"
+                  label="인증번호"
+                  required
+                  fullWidth
+                  autoFocus
+                  onChange={(e) => setIsCheckedEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Button
+                  sx={{
+                    bgcolor: "#000d3e",
+                    color: "#FFF",
+                  }}
+                  onClick={() => {
+                    handleModalShow()
+                    onClickEmailAuthentication()
+                  }}
+                >
+                  확인
+                </Button>
+              </Grid>
+              {!isCheckedEmail && (
+                <Grid item xs={12} className="text-success">
+                  인증되지 않은 이메일입니다
+                </Grid>
+              )}
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="firstName"
+                  id="firstName"
+                  label="이름"
+                  required
+                  fullWidth
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="lastName"
+                  id="lastName"
+                  label="성"
+                  required
+                  fullWidth
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="nickName"
+                  id="nickName"
+                  label="닉네임"
+                  required
+                  fullWidth
+                  autoFocus
+                  onChange={(e) => setNickname(e.target.value)}
+                />
+                {!isNicknameValid && (
+                  <span className="text-success">
+                    닉네임은 2글자 이상으로 설정해 주세요.
+                  </span>
+                )}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  id="password"
+                  label="비밀번호"
+                  required
+                  fullWidth
+                  autoFocus
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {!isPasswordValid && (
+                  <span className="text-success">
+                    비밀번호는 4글자 이상으로 설정해 주세요.
+                  </span>
+                )}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="passwordCheck"
+                  id="passwordCheck"
+                  label="비밀번호 재확인"
+                  required
+                  fullWidth
+                  autoFocus
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                {!isPasswordSame && (
+                  <span className="text-success">
+                    비밀번호가 일치하지 않습니다.
+                  </span>
+                )}
+              </Grid>
+            </Grid>
+
+            {/* submit and sign-in button */}
+            <Button
+              type="submit"
+              variant="contained"
+              fullWidth
+              sx={{ mt: 2, mb: 2, bgcolor: "#000d3e" }}
+            >
+              가입하기
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Link href="../login" variant="body2">
+                이미 가입하셨나요? 로그인하기
+              </Link>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+      {/* <Row className="justify-content-md-center mt-5">
           <Col lg={8}>
             <Form onSubmit={handleSubmit}>
               <Form.Group controlId="registerEmail" class="me-3">
@@ -221,8 +414,7 @@ function RegisterForm() {
               </Form.Group>
             </Form>
           </Col>
-        </Row>
-      </Container>
+        </Row> */}
 
       {isModalActive && (
         <EmailAuthModal
@@ -232,7 +424,7 @@ function RegisterForm() {
           isCheckedEmailCallback={isCheckedEmailCallback}
         />
       )}
-    </>
+    </ThemeProvider>
   )
 }
 
