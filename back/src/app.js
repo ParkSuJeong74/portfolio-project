fccfe251b96c4193686e8507a28f62f1dc2fc91d
ParkSuJeong <sujeong9158@gmail.com>
2022-04-10 15:18:11 +0000

    dockerize

--- back/src/app.js
+++ back/src/app.js
@@ -15,7 +15,7 @@ const { errorMiddleware } = require("./middlewares/errorMiddleware")
 const app = express()
 // CORS 에러 방지
 
-app.use(cors({ origin: 'http://elice-kdt-ai-4th-team21.elicecoding.com', optionsSuccessStatus: 200 } ))
+app.use(cors())
 // express 기본 제공 middleware
 // express.json(): POST 등의 요청과 함께 오는 json형태의 데이터를 인식하고 핸들링할 수 있게 함.
 // express.urlencoded: 주로 Form submit 에 의해 만들어지는 URL-Encoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
