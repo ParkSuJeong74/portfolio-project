3d9245abd449e35563a24d7fbab3a5c76e59e337
rnrn99 <giu0213@naver.com>
2022-04-13 11:36:16 +0900

    feat : 캘린더 기능 세팅

--- front/src/components/calendar/Calendar.js
+++ front/src/components/calendar/Calendar.js
@@ -1,10 +1,58 @@
 import React from "react"
-import FullCalendar from "@fullcalendar/react" // must go before plugins
-import dayGridPlugin from "@fullcalendar/daygrid" // a plugin!
-import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import listPlugin from "@fullcalendar/list"
+import interactionPlugin from "@fullcalendar/interaction"
+import "./Calendar.css"
 
 function Calendar() {
-  return <FullCalendar plugins={[dayGridPlugin, interactionPlugin]} />
+  const fakeData = [
+    {
+      title: "안녕",
+      start: "2022-04-01",
+      end: "2022-04-04",
+      borderColor: "red",
+      backgroundColor: "red",
+    },
+    { title: "event 2", start: "2022-04-02" },
+    {
+      title: "event 4",
+      start: "2022-04-10T14:50:27",
+      end: "2022-04-17T12:30:00",
+      allDay: true,
+    },
+    {
+      title: "event3",
+      start: "2022-04-10T14:50:27",
+      allDay: false,
+      textColor: "red",
+      backgroundColor: "red",
+    },
+  ]
+  return (
+    <>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          left: "title",
+          right: "today prev,next",
+        }}
+        editable={true}
+        selectable={true}
+        events={fakeData}
+      />
+      <FullCalendar
+        plugins={[listPlugin, interactionPlugin]}
+        initialView="listWeek"
+        headerToolbar={{
+          left: "title",
+          right: "prev,next",
+        }}
+        editable={true}
+        events={fakeData}
+      />
+    </>
+  )
 }
 
 export default Calendar
