917542f400fc2e0247fe3479d25494073734a15d
rnrn99 <giu0213@naver.com>
2022-04-13 19:47:28 +0900

    chore : award api 수정

--- front/src/components/award/AwardCard.js
+++ front/src/components/award/AwardCard.js
@@ -9,73 +9,82 @@ import DeleteIcon from "@mui/icons-material/Delete"
 import * as Api from "../../api"
 
 function AwardCard({ award, isEditable, setIsEditing, setAwards }) {
-  const userState = useSelector((state) => state.user) // 현재 로그인된 유저의 정보를 가져옵니다.
-  const [anchorEl, setAnchorEl] = useState(null) // Menu Element를 가리킵니다.
-  const [isOpen, setIsOpen] = useState(null) // Menu Element의 Open 상태를 저장합니다.
+    const userState = useSelector((state) => state.user) // 현재 로그인된 유저의 정보를 가져옵니다.
+    const [anchorEl, setAnchorEl] = useState(null) // Menu Element를 가리킵니다.
+    const [isOpen, setIsOpen] = useState(null) // Menu Element의 Open 상태를 저장합니다.
 
-  const userId = userState.user.id // 수상이력 리스트를 갱신하기 위해 유저 아이디를 저장합니다.
+    const userId = userState.user.id // 수상이력 리스트를 갱신하기 위해 유저 아이디를 저장합니다.
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
-    setIsOpen(Boolean(event.currentTarget))
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-    setIsOpen(false)
-  }
-
-  // 수상이력 삭제 핸들러입니다.
-  const removeAward = async () => {
-    try {
-      // award로 DELETE 요청을 보내 수상이력을 삭제합니다.
-      await Api.delete("award", award.id)
-
-      // award/list/유저id로 GET 요청을 보내 업데이트 사항이 반영된 수상이력을 새로 저장합니다.
-      const res = await Api.get("award/list", userId)
-      setAwards(res.data)
-    } catch (error) {
-      alert(error.response.data)
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
+        setIsOpen(Boolean(event.currentTarget))
     }
-  }
+    const handleClose = () => {
+        setAnchorEl(null)
+        setIsOpen(false)
+    }
 
-  return (
-    <Grid container spacing={2}>
-      <Grid item xs={10}>
-        <p style={{ marginBottom: "5px" }}>{award.title}</p>
-        <p className="text-muted">{award.description}</p>
-      </Grid>
-      <Grid item xs={2}>
-        {isEditable && (
-          <>
-            <IconButton onClick={handleClick} sx={{ float: "right", mb: 2 }}>
-              <MoreHorizIcon />
-            </IconButton>
-            {isOpen && (
-              <Menu anchorEl={anchorEl} open={isOpen} onClose={handleClose}>
-                <MenuItem onClick={handleClose}>
-                  <Button
-                    onClick={() => setIsEditing((cur) => !cur)}
-                    startIcon={<EditIcon />}
-                  >
-                    편집
-                  </Button>
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                  <Button
-                    color="error"
-                    startIcon={<DeleteIcon />}
-                    onClick={removeAward}
-                  >
-                    삭제
-                  </Button>
-                </MenuItem>
-              </Menu>
-            )}
-          </>
-        )}
-      </Grid>
-    </Grid>
-  )
+    // 수상이력 삭제 핸들러입니다.
+    const removeAward = async () => {
+        try {
+            // award로 DELETE 요청을 보내 수상이력을 삭제합니다.
+            await Api.delete("awards", award.id)
+
+            // award/list/유저id로 GET 요청을 보내 업데이트 사항이 반영된 수상이력을 새로 저장합니다.
+            const res = await Api.get("awards/lists", userId)
+            setAwards(res.data)
+        } catch (error) {
+            alert(error.response.data)
+        }
+    }
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={10}>
+                <p style={{ marginBottom: "5px" }}>{award.title}</p>
+                <p className="text-muted">{award.description}</p>
+            </Grid>
+            <Grid item xs={2}>
+                {isEditable && (
+                    <>
+                        <IconButton
+                            onClick={handleClick}
+                            sx={{ float: "right", mb: 2 }}
+                        >
+                            <MoreHorizIcon />
+                        </IconButton>
+                        {isOpen && (
+                            <Menu
+                                anchorEl={anchorEl}
+                                open={isOpen}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>
+                                    <Button
+                                        onClick={() =>
+                                            setIsEditing((cur) => !cur)
+                                        }
+                                        startIcon={<EditIcon />}
+                                    >
+                                        편집
+                                    </Button>
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <Button
+                                        color="error"
+                                        startIcon={<DeleteIcon />}
+                                        onClick={removeAward}
+                                    >
+                                        삭제
+                                    </Button>
+                                </MenuItem>
+                            </Menu>
+                        )}
+                    </>
+                )}
+            </Grid>
+        </Grid>
+    )
 }
 
 export default AwardCard
--- front/src/components/award/Awards.js
+++ front/src/components/award/Awards.js
@@ -1,135 +1,141 @@
 import React, { useEffect, useState } from "react"
 
 import {
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Card,
-  CardContent,
-  Typography,
-  IconButton,
-  Box,
-  Dialog,
-  DialogTitle,
-  DialogContent,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+    Card,
+    CardContent,
+    Typography,
+    IconButton,
+    Box,
+    Dialog,
+    DialogTitle,
+    DialogContent,
 } from "@mui/material"
 import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded"
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import CloseIcon from "@mui/icons-material/Close"
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents"
 
 import * as Api from "../../api"
 import Award from "./Award"
 import AwardAddForm from "./AwardAddForm"
 
 function Awards({ portfolioOwnerId, isEditable }) {
-  const [awards, setAwards] = useState([])
-  const [isAdding, setIsAdding] = useState(false)
+    const [awards, setAwards] = useState([])
+    const [isAdding, setIsAdding] = useState(false)
 
-  useEffect(() => {
-    Api.get("award/list", portfolioOwnerId).then((res) => setAwards(res.data))
-  }, [portfolioOwnerId])
+    useEffect(() => {
+        Api.get("awards/lists", portfolioOwnerId).then((res) =>
+            setAwards(res.data),
+        )
+    }, [portfolioOwnerId])
 
-  return (
-    <Card sx={{ marginBottom: "20px", borderRadius: "15px" }}>
-      <Accordion defaultExpanded={true} sx={{ boxShadow: 0 }}>
-        <AccordionSummary
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          style={{padding: 0}}
-        >
-          <Box 
-            sx={{ 
-              width: '100%', 
-              display: 'flex', 
-              justifyContent: 'space-between', 
-            }}>
-          
-            <Box sx={{ display: 'flex', marginLeft: '20px'}}>
-              <EmojiEventsIcon sx={{fontSize: "2.8em"}}/>
-              <Typography
-                sx={{
-                  fontFamily: "Elice Digital Baeum",
-                  fontSize: "1.8em",
-                  color: "#08075C",
-                  fontWeight: 800,
-                  marginLeft: "15px",
-                }}
-              >
-                Award
-              </Typography>
-            </Box>
-          
-            <Typography
-              sx={{
-                marginTop: '10px',
-                width: '500px',
-                height: '20px',
-                backgroundColor: '#6D55FF',
-              }} />
-        </Box>
-
-        </AccordionSummary>
-        <AccordionDetails>
-          {awards &&
-            awards.map((award) => (
-              <Award
-                key={award.id}
-                award={award}
-                setAwards={setAwards}
-                isEditable={isEditable}
-              />
-            ))}
-        </AccordionDetails>
-      </Accordion>
-
-      {isEditable && (
-        <CardContent>
-          <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <IconButton
-              style={{ color: "#08075C" }}
-              aria-label="add-award"
-              onClick={() => setIsAdding((cur) => !cur)}
-            >
-              <AddCircleRoundedIcon sx={{ width: "56px", height: "56px" }} />
-            </IconButton>
-          </Box>
-
-          {isAdding && (
-            <Dialog open={isAdding} onClose={() => setIsAdding((cur) => !cur)}>
-              <DialogTitle
-                sx={{
-                  fontFamily: "Elice Digital Baeum",
-                  fontWeight: 500,
-                  fontSize: "1.5rem",
-                }}
-              >
-                <IconButton
-                  onClick={() => setIsAdding((cur) => !cur)}
-                  sx={{
-                    position: "absolute",
-                    right: 10,
-                    top: 10,
-                    color: "#9e9e9e",
-                  }}
+    return (
+        <Card sx={{ marginBottom: "20px", borderRadius: "15px" }}>
+            <Accordion defaultExpanded={true} sx={{ boxShadow: 0 }}>
+                <AccordionSummary
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    style={{ padding: 0 }}
                 >
-                  <CloseIcon />
-                </IconButton>
-                수상이력 추가
-              </DialogTitle>
-              <DialogContent>
-                <AwardAddForm
-                  portfolioOwnerId={portfolioOwnerId}
-                  setIsAdding={setIsAdding}
-                  setAwards={setAwards}
-                />
-              </DialogContent>
-            </Dialog>
-          )}
-        </CardContent>
-      )}
-    </Card>
-  )
+                    <Box
+                        sx={{
+                            width: "100%",
+                            display: "flex",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <Box sx={{ display: "flex", marginLeft: "20px" }}>
+                            <EmojiEventsIcon sx={{ fontSize: "2.8em" }} />
+                            <Typography
+                                sx={{
+                                    fontFamily: "Elice Digital Baeum",
+                                    fontSize: "1.8em",
+                                    color: "#08075C",
+                                    fontWeight: 800,
+                                    marginLeft: "15px",
+                                }}
+                            >
+                                Award
+                            </Typography>
+                        </Box>
+
+                        <Typography
+                            sx={{
+                                marginTop: "10px",
+                                width: "500px",
+                                height: "20px",
+                                backgroundColor: "#6D55FF",
+                            }}
+                        />
+                    </Box>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {awards &&
+                        awards.map((award) => (
+                            <Award
+                                key={award.id}
+                                award={award}
+                                setAwards={setAwards}
+                                isEditable={isEditable}
+                            />
+                        ))}
+                </AccordionDetails>
+            </Accordion>
+
+            {isEditable && (
+                <CardContent>
+                    <Box sx={{ display: "flex", justifyContent: "center" }}>
+                        <IconButton
+                            style={{ color: "#08075C" }}
+                            aria-label="add-award"
+                            onClick={() => setIsAdding((cur) => !cur)}
+                        >
+                            <AddCircleRoundedIcon
+                                sx={{ width: "56px", height: "56px" }}
+                            />
+                        </IconButton>
+                    </Box>
+
+                    {isAdding && (
+                        <Dialog
+                            open={isAdding}
+                            onClose={() => setIsAdding((cur) => !cur)}
+                        >
+                            <DialogTitle
+                                sx={{
+                                    fontFamily: "Elice Digital Baeum",
+                                    fontWeight: 500,
+                                    fontSize: "1.5rem",
+                                }}
+                            >
+                                <IconButton
+                                    onClick={() => setIsAdding((cur) => !cur)}
+                                    sx={{
+                                        position: "absolute",
+                                        right: 10,
+                                        top: 10,
+                                        color: "#9e9e9e",
+                                    }}
+                                >
+                                    <CloseIcon />
+                                </IconButton>
+                                수상이력 추가
+                            </DialogTitle>
+                            <DialogContent>
+                                <AwardAddForm
+                                    portfolioOwnerId={portfolioOwnerId}
+                                    setIsAdding={setIsAdding}
+                                    setAwards={setAwards}
+                                />
+                            </DialogContent>
+                        </Dialog>
+                    )}
+                </CardContent>
+            )}
+        </Card>
+    )
 }
 
 export default Awards
