335076d399dec5b738e11e8f4063001f109d027b
ParkSuJeong <sujeong9158@gmail.com>
2022-04-14 23:10:11 +0900

    refactoring:follow api userIdYour -> targetUserId

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -9,187 +9,190 @@ const userAuthRouter = Router()
 
 // 이메일 입력 -> 인증코드 발송하고 front로 인증코드 send
 userAuthRouter.post("/emailAuth", async (req, res, next) => {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        "headers의 Content-Type을 application/json으로 설정해주세요"
-      )
+    try {
+        if (is.emptyObject(req.body)) {
+            throw new Error(
+                "headers의 Content-Type을 application/json으로 설정해주세요"
+            )
+        }
+
+        const { email } = req.body
+        // 이메일 중복검사
+        await userAuthService.isExistUser({ email })
+        const message = `<p>회원가입을 위한 인증번호입니다.</p>`
+        const authCode = await emailUtil.sendEmail(email, message)
+
+        res.status(200).send(`${authCode}`)
+    } catch (error) {
+        next(error)
     }
-
-    const { email } = req.body
-    // 이메일 중복검사
-    await userAuthService.isExistUser({ email })
-    const message = `<p>회원가입을 위한 인증번호입니다.</p>`
-    const authCode = await emailUtil.sendEmail(email, message)
-
-    res.status(200).send(`${authCode}`)
-  } catch (error) {
-    next(error)
-  }
 })
 
 // 회원가입
 userAuthRouter.post("/register", async (req, res, next) => {
-  try {
-    const { name, nickname, email, password } = req.body
-    await userAuthService.isExistUser({ email })
-    const newUser = await userAuthService.addUser({
-      name,
-      nickname,
-      email,
-      password,
-    })
+    try {
+        const { name, nickname, email, password } = req.body
+        await userAuthService.isExistUser({ email })
+        const newUser = await userAuthService.addUser({
+            name,
+            nickname,
+            email,
+            password,
+        })
 
-    res.status(201).json(newUser)
-  } catch (error) {
-    next(error)
-  }
+        res.status(201).json(newUser)
+    } catch (error) {
+        next(error)
+    }
 })
 
 // 로그인
 userAuthRouter.post("/login", async (req, res, next) => {
-  try {
-    const { email, password } = req.body
-    const user = await userAuthService.getUser({ email, password })
+    try {
+        const { email, password } = req.body
+        const user = await userAuthService.getUser({ email, password })
 
-    res.status(200).send(user)
-  } catch (error) {
-    next(error)
-  }
+        res.status(200).send(user)
+    } catch (error) {
+        next(error)
+    }
 })
 
 // user list
 userAuthRouter.get("/lists", login_required, async (req, res, next) => {
-  try {
-    const users = await userAuthService.getUsers()
-    res.status(200).send(users)
-  } catch (error) {
-    next(error)
-  }
+    try {
+        const users = await userAuthService.getUsers()
+        res.status(200).send(users)
+    } catch (error) {
+        next(error)
+    }
 })
 
 // login session
 userAuthRouter.get("/current", login_required, async (req, res, next) => {
-  try {
-    // jwt토큰에서 추출된 사용자 id를 가지고 db에서 사용자 정보를 찾음.
-    const userId = req.currentUserId
-    const currentUserInfo = await userAuthService.getUserInfo({
-      userId,
-    })
+    try {
+        // jwt토큰에서 추출된 사용자 id를 가지고 db에서 사용자 정보를 찾음.
+        const userId = req.currentUserId
+        const currentUserInfo = await userAuthService.getUserInfo({
+            userId,
+        })
 
-    res.status(200).send(currentUserInfo)
-  } catch (error) {
-    next(error)
-  }
+        res.status(200).send(currentUserInfo)
+    } catch (error) {
+        next(error)
+    }
 })
 
 // put user info
 userAuthRouter.put("/:id", login_required, async (req, res, next) => {
-  try {
-    const userId = req.params.id
-    if (userId != req.currentUserId) {
-      throw new Error("본인이 아니면 사용자 정보를 편집할 수 없습니다.")
-    } else {
-      const { nickname, description } = req.body
-      const toUpdate = { nickname, description }
+    try {
+        const userId = req.params.id
+        if (userId != req.currentUserId) {
+            throw new Error("본인이 아니면 사용자 정보를 편집할 수 없습니다.")
+        } else {
+            const { nickname, description } = req.body
+            const toUpdate = { nickname, description }
 
-      const updatedUser = await userAuthService.setUser({ userId, toUpdate })
+            const updatedUser = await userAuthService.setUser({
+                userId,
+                toUpdate,
+            })
 
-      res.status(200).json(updatedUser)
+            res.status(200).json(updatedUser)
+        }
+    } catch (error) {
+        next(error)
     }
-  } catch (error) {
-    next(error)
-  }
 })
 
 // get user info
 userAuthRouter.get("/:id", login_required, async (req, res, next) => {
-  try {
-    const userId = req.params.id
-    const currentUserInfo = await userAuthService.getUserInfo({ userId })
+    try {
+        const userId = req.params.id
+        const currentUserInfo = await userAuthService.getUserInfo({ userId })
 
-    res.status(200).send(currentUserInfo)
-  } catch (error) {
-    next(error)
-  }
+        res.status(200).send(currentUserInfo)
+    } catch (error) {
+        next(error)
+    }
 })
 
 userAuthRouter.delete("/:id", login_required, async (req, res, next) => {
-  try {
-    const userId = req.params.id
-    const result = await userAuthService.deleteUser({ userId })
+    try {
+        const userId = req.params.id
+        const result = await userAuthService.deleteUser({ userId })
 
-    res.status(200).send(result)
-  } catch (error) {
-    next(error)
-  }
+        res.status(200).send(result)
+    } catch (error) {
+        next(error)
+    }
 })
 
 userAuthRouter.put("/follow/:id", login_required, async (req, res, next) => {
-  try {
-    const userIdMy = req.params.id
-    const { userIdYour } = req.body
+    try {
+        const userId = req.params.id
+        const { targetUserId } = req.body
 
-    const updatedUsers = await userAuthService.setUserFollow({
-      userIdYour,
-      userIdMy,
-    })
+        const updatedUsers = await userAuthService.setUserFollow({
+            targetUserId,
+            userId,
+        })
 
-    res.status(200).json(updatedUsers)
-  } catch (error) {
-    next(error)
-  }
+        res.status(200).json(updatedUsers)
+    } catch (error) {
+        next(error)
+    }
 })
 
 userAuthRouter.post(
-  "/:id/img",
-  login_required,
-  s3Upload(),
-  async (req, res, next) => {
-    try {
-      const userId = req.params.id
-      const userInfo = await userAuthService.getUserInfo({ userId })
+    "/:id/img",
+    login_required,
+    s3Upload(),
+    async (req, res, next) => {
+        try {
+            const userId = req.params.id
+            const userInfo = await userAuthService.getUserInfo({ userId })
 
-      if (userInfo.imageName !== "none") {
-        s3Delete(userInfo.imageName)
-      }
+            if (userInfo.imageName !== "none") {
+                s3Delete(userInfo.imageName)
+            }
 
-      const { location } = req.file
-      const imageName = location.split("amazonaws.com/")[1]
-      const updatedUser = await userAuthService.setUserImage({
-        userId,
-        imageName,
-      })
+            const { location } = req.file
+            const imageName = location.split("amazonaws.com/")[1]
+            const updatedUser = await userAuthService.setUserImage({
+                userId,
+                imageName,
+            })
 
-      res.status(200).json({ updatedUser })
-    } catch (error) {
-      next(error)
+            res.status(200).json({ updatedUser })
+        } catch (error) {
+            next(error)
+        }
     }
-  }
 )
 
 userAuthRouter.post(
-  "/:id/img/delete",
-  login_required,
-  async (req, res, next) => {
-    try {
-      const userId = req.params.id
-      const userInfo = await userAuthService.getUserInfo({ userId })
+    "/:id/img/delete",
+    login_required,
+    async (req, res, next) => {
+        try {
+            const userId = req.params.id
+            const userInfo = await userAuthService.getUserInfo({ userId })
 
-      if (userInfo.imageName !== "none") {
-        s3Delete(userInfo.imageName)
-      }
+            if (userInfo.imageName !== "none") {
+                s3Delete(userInfo.imageName)
+            }
 
-      const updatedUser = await userAuthService.setUserImage({
-        userId,
-        imageName: "none",
-      })
+            const updatedUser = await userAuthService.setUserImage({
+                userId,
+                imageName: "none",
+            })
 
-      res.status(200).json({ updatedUser })
-    } catch (error) {
-      next(error)
+            res.status(200).json({ updatedUser })
+        } catch (error) {
+            next(error)
+        }
     }
-  }
 )
 
 module.exports = { userAuthRouter }
--- front/src/components/user/LoginForm.js
+++ front/src/components/user/LoginForm.js
@@ -1,194 +1,291 @@
-import  { useState } from "react"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useDispatch } from "react-redux"
-import {Col, Row, Form } from "react-bootstrap"
+import { Col, Row, Form } from "react-bootstrap"
 
 import * as Api from "../../api"
 import { login } from "../../redux/action/userAction"
 
-import PasswordChangeModal from "./PasswordChangeModal"
+import * as React from "react"
+import Avatar from "@mui/material/Avatar"
+import Button from "@mui/material/Button"
+import IconButton from "@mui/material/Button"
+import CssBaseline from "@mui/material/CssBaseline"
+import TextField from "@mui/material/TextField"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import Checkbox from "@mui/material/Checkbox"
+import Link from "@mui/material/Link"
+import Grid from "@mui/material/Grid"
+import Box from "@mui/material/Box"
 
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-
+import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank"
 
 function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="localhost:3000">
-        PUPA
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography
+            variant="body2"
+            color="text.secondary"
+            align="center"
+            {...props}
+        >
+            {"Copyright © "}
+            <Link color="inherit" href="localhost:3000">
+                PUPA
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    )
 }
 
-const theme = createTheme();
+const theme = createTheme()
 
 function LoginForm() {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-  const validateEmail = (email) => {
-    return email
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      )
-  }
-
-  const isEmailValid = validateEmail(email)
-  const isPasswordValid = password.length >= 4
-
-  // const isFormValid = isEmailValid && isPasswordValid
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    try {
-      const res = await Api.post("users/login", {
-        email,
-        password,
-      })
-      const user = res.data
-      const jwtToken = user.token
-      sessionStorage.setItem("userToken", jwtToken)
-
-      dispatch(login(user))
-      navigate("/", { replace: true })
-    } catch (error) {
-      alert(error.response.data)
+    const validateEmail = (email) => {
+        return email
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            )
     }
-  }
 
-  const [isModalActive, setIsModalActive] = useState(false)
+    const isEmailValid = validateEmail(email)
+    const isPasswordValid = password.length >= 4
 
-  const handleModalClose = () => setIsModalActive(false)
-  const handleModalShow = () => setIsModalActive(true)
+    // const isFormValid = isEmailValid && isPasswordValid
 
-  return (
-    <>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: '#08075C' }}>
-              <img src={process.env.PUBLIC_URL + '/only_butterflies.png'} style={{width: '80px', height: '40px'}} />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                style={{backgroundColor: '#08075C'}}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link onClick={()=>{ handleModalShow()}} variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link onClick={()=>navigate("/register")} variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-              
-                <Row className= 'mt-3'>
-                  <Col>
-                    <IconButton sx={{ minWidth: '30px', width: '30px', height: '30px' }}>
-                      <img src={process.env.PUBLIC_URL + '/google.png'} style={{ width: 'inherit', height: 'inherit' }} />
-                    </IconButton>
-                  </Col>
+    const handleSubmit = async (e) => {
+        e.preventDefault()
 
-                  <Col>
-                    <IconButton sx={{ minWidth: '30px', width: '30px', height: '30px' }}>
-                      <img src={process.env.PUBLIC_URL + '/kakao.png'} style={{width: '30px', height: '30px'}} />
-                    </IconButton>
-                  </Col>
+        try {
+            const res = await Api.post("users/login", {
+                email,
+                password,
+            })
+            const user = res.data
+            const jwtToken = user.token
+            sessionStorage.setItem("userToken", jwtToken)
 
-                  <Col>
-                    <IconButton sx={{ minWidth: '30px', width: '30px', height: '30px' }}>
-                      <img src={process.env.PUBLIC_URL + '/naver.png'} style={{width: '40px', height: '40px'}} />
-                    </IconButton>
-                  </Col>
+            dispatch(login(user))
+            navigate("/", { replace: true })
+        } catch (error) {
+            alert(error.response.data)
+        }
+    }
 
-                  <Col>
-                    <IconButton sx={{ minWidth: '30px', width: '30px', height: '30px' }}>
-                      <img src={process.env.PUBLIC_URL + '/github.png'} style={{width: '30px', height: '30px'}} />
-                    </IconButton>
-                  </Col>
+    const [isModalActive, setIsModalActive] = useState(false)
 
-                  <Col>
-                    <IconButton sx={{ minWidth: '30px', width: '30px', height: '30px' }}>
-                      <img src={process.env.PUBLIC_URL + '/facebook.png'} style={{width: '30px', height: '30px'}} />
-                    </IconButton>
-                  </Col>
-                </Row> 
-            </Box>
-          </Box>
-          <Copyright sx={{ mt: 8, mb: 4 }} />
-        </Container>
-    </>
-  )
+    const handleModalClose = () => setIsModalActive(false)
+    const handleModalShow = () => setIsModalActive(true)
+
+    return (
+        <>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: "#08075C" }}>
+                        <img
+                            src={
+                                process.env.PUBLIC_URL + "/only_butterflies.png"
+                            }
+                            style={{ width: "80px", height: "40px" }}
+                        />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        noValidate
+                        sx={{ mt: 1 }}
+                    >
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox value="remember" color="primary" />
+                            }
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            style={{ backgroundColor: "#08075C" }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link
+                                    onClick={() => {
+                                        handleModalShow()
+                                    }}
+                                    variant="body2"
+                                >
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link
+                                    onClick={() => navigate("/register")}
+                                    variant="body2"
+                                >
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+
+                        <Row className="mt-3">
+                            <Col>
+                                <IconButton
+                                    sx={{
+                                        minWidth: "30px",
+                                        width: "30px",
+                                        height: "30px",
+                                    }}
+                                >
+                                    <img
+                                        src={
+                                            process.env.PUBLIC_URL +
+                                            "/google.png"
+                                        }
+                                        style={{
+                                            width: "inherit",
+                                            height: "inherit",
+                                        }}
+                                    />
+                                </IconButton>
+                            </Col>
+
+                            <Col>
+                                <IconButton
+                                    sx={{
+                                        minWidth: "30px",
+                                        width: "30px",
+                                        height: "30px",
+                                    }}
+                                >
+                                    <img
+                                        src={
+                                            process.env.PUBLIC_URL +
+                                            "/kakao.png"
+                                        }
+                                        style={{
+                                            width: "30px",
+                                            height: "30px",
+                                        }}
+                                    />
+                                </IconButton>
+                            </Col>
+
+                            <Col>
+                                <IconButton
+                                    sx={{
+                                        minWidth: "30px",
+                                        width: "30px",
+                                        height: "30px",
+                                    }}
+                                >
+                                    <img
+                                        src={
+                                            process.env.PUBLIC_URL +
+                                            "/naver.png"
+                                        }
+                                        style={{
+                                            width: "40px",
+                                            height: "40px",
+                                        }}
+                                    />
+                                </IconButton>
+                            </Col>
+
+                            <Col>
+                                <IconButton
+                                    sx={{
+                                        minWidth: "30px",
+                                        width: "30px",
+                                        height: "30px",
+                                    }}
+                                >
+                                    <img
+                                        src={
+                                            process.env.PUBLIC_URL +
+                                            "/github.png"
+                                        }
+                                        style={{
+                                            width: "30px",
+                                            height: "30px",
+                                        }}
+                                    />
+                                </IconButton>
+                            </Col>
+
+                            <Col>
+                                <IconButton
+                                    sx={{
+                                        minWidth: "30px",
+                                        width: "30px",
+                                        height: "30px",
+                                    }}
+                                >
+                                    <img
+                                        src={
+                                            process.env.PUBLIC_URL +
+                                            "/facebook.png"
+                                        }
+                                        style={{
+                                            width: "30px",
+                                            height: "30px",
+                                        }}
+                                    />
+                                </IconButton>
+                            </Col>
+                        </Row>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </>
+    )
 }
 
 export default LoginForm
